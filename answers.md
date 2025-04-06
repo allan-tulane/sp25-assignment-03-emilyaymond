e9dac64a6316b285a85861396f03ccf8d439ffb6
emilyaymond <emilydrew16@yahoo.com>
2025-04-06 15:47:04 -0500

    updated

--- answers.md
+++ answers.md
@@ -1,8 +1,26 @@
 # CMPS 2200 Assignment 3
 ## Answers
 
-**Name:**_________________________
+**Name:** Emily Aymond
 
 
 Place all written answers from `assignment-03.md` here for easier grading.
 
+1a. Find the largest 2^k so that 2^k <= N. Then, subtract 2^k from N and keep subtracting 2^k until N <= 1. 
+
+
+1b. This algorithm is optimal because k is at its largest possible value every time. We know this because the largest value one of our coins could have is always less than or equal to N, so there are no smaller coins to replace it without using more coins.
+
+
+1c. Work: O(log n)
+
+Span: O(log n)
+
+
+2a. Consider D = {1, 3, 4} and N = 6. The greedy algorithm would choose 4 first, which would leave 2, forcing two ones to be used. This totals three coins. However, the most efficient way to do this would be to use two threes, using only two coins.
+
+
+2b. This is an optimal substructure because it relies on solving smaller problems to take on larger ones
+
+
+2c. In the top-down method, we have to take into account all of the substructures, so this makes the work and span O(n).
--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import math, queue
-from collections import Counter
 
 ####### Problem 3 #######
 
-test_cases = [('book', 'back'), ('kookaburra', 'kookybird'), ('elephant', 'relevant'), ('AAAGAATTCA', 'AAATCA')]
-alignments = [('b--ook', 'bac--k'), ('kook-ab-urr-a', 'kooky-bi-r-d-'), ('relev--ant','-ele-phant'), ('AAAGAATTCA', 'AAA---T-CA')]
+test_cases = [('book', 'badistk'), ('kookaburra', 'kookybird'), ('elephant', 'relevant'), ('AAAGAATTdistA', 'AAATdistA')]
+alignments = [('b--ook', 'badist--k'), ('kook-ab-urr-a', 'kooky-bi-r-d-'), ('relev--ant','-ele-phant'), ('AAAGAATTdistA', 'AAA---T-distA')]
 
 def MED(S, T):
-    # TO DO - modify to account for insertions, deletions and substitutions
+    # TO DO - modify to adistount for insertions, deletions and substitutions
     if (S == ""):
         return(len(T))
     elif (T == ""):
@@ -18,12 +17,57 @@ def MED(S, T):
         else:
             return(1 + min(MED(S, T[1:]), MED(S[1:], T)))
 
+# for S, T in test_cases:
+#     print(MED(S, T))
+def fast_MED(S, T, MED=None):
+    if MED is None:
+        MED = {}
+    if (S, T) in MED:
+        return MED[(S, T)]
+    if S == "":
+        return len(T)
+    if T == "":
+        return len(S)
+    elif S[0] == T[0]:
+        MED[(S, T)] = fast_MED(S[1:], T[1:], MED)
+    else:
+        MED[(S, T)] = 1+ min(fast_MED(S, T[1:], MED),
+                             fast_MED(S[1:], T, MED))
 
-def fast_MED(S, T, MED={}):
-    # TODO -  implement top-down memoization
-    pass
+    return MED[(S, T)]
+
 
 def fast_align_MED(S, T, MED={}):
-    # TODO - keep track of alignment
-    pass
+    if (S, T) in MED:
+        return MED[(S, T)]
 
+    if S == "":
+        return ("-" * len(T), T)
+
+    if T == "":
+        return (S, "-" * len(S))
+
+    if S[0] == T[0]:
+        align_S, align_T = fast_align_MED(S[1:], T[1:], MED)
+        result = (S[0] + align_S, T[0] + align_T)
+
+    else:
+        ins_S, ins_T = fast_align_MED(S, T[1:], MED)
+        del_S, del_T = fast_align_MED(S[1:], T, MED)
+
+        ins_C = 1 + len(ins_S)
+        del_C = 1 + len(del_S)
+
+        if ins_C <= del_C:
+            result = ("-" + ins_S, T[0] + ins_T)
+        else:
+            result = (S[0] + del_S, "-" + del_T)
+    MED[(S, T)] = result
+    return result
+
+
+
+# print(MED(test_cases[0][0], test_cases[0][1]));
+
+
+print(fast_align_MED('book', 'badistk'))
